7c4c3a03fa911b0907cf589213b53fff2ec8a971
deltamaze <wcpooley@gmail.com>
2019-01-18 23:36:36 -0600

    unit testing around dal

---
+++ HelloWorld/HelloWorldLibrary/DataAccess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using HelloWorldLibrary.Model;
+
+namespace HelloWorldLibrary
+{
+    class DataAccess
+    {
+        private static string personTextFIle = "PersonText.txt";
+
+        public static void AddNewPerson(PersonModel person)
+        {
+            List<PersonModel> people = GetAllPeople();
+
+            AddPersonToPeopleList(people, person);
+
+            List<string> lines = ConvertModelsToCsv(people);
+            File.WriteAllLines(personTextFIle, lines);
+
+            File.WriteAllLines(personTextFIle, lines);
+
+        }
+
+        public static void AddPersonToPeopleList(List<PersonModel> people, PersonModel person)
+        {
+            people.Add(person);
+        }
+
+        public static List<string> ConvertModelsToCsv(List<PersonModel> people)
+        {
+            List<string> output = new List<string>();
+            foreach (PersonModel user in people)
+            {
+                output.Add($"{user.Name}");
+            }
+            return output;
+            
+        }
+
+        public static List<PersonModel> GetAllPeople()
+        {
+
+            List<PersonModel> output = new List<PersonModel>();
+            string[] content = File.ReadAllLines(personTextFIle);
+
+            foreach(string line in content)
+            {
+                output.Add(new PersonModel { Name = line });
+            }
+            return output;
+        }
+    }
+}
---
+++ HelloWorld/HelloWorldLibrary/Model/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldLibrary.Model
+{
+    class PersonModel
+    {
+        public string Name { get; set; }
+    }
+}
